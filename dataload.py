e337efd1d2ea685eeae172e62ce26a1d20a2acaf
AgentGuo <841796600@qq.com>
2021-02-05 20:33:07 +0800

    finish the patten-key attack

--- dataload.py
+++ dataload.py
@@ -3,7 +3,7 @@ from torch.utils.data import DataLoader
 import cv2
 import numpy as np
 import torch as t
-class posionDataset(t.utils.data.Dataset):
+class InstanceKeyposionDataset(t.utils.data.Dataset):
     def __init__(self, imgPath, label, size, bias):
         np.random.seed(0)
         img = cv2.resize(cv2.imread(imgPath, 0), (28, 28)).reshape(1, 28, 28).astype(np.float32)
@@ -11,6 +11,17 @@ class posionDataset(t.utils.data.Dataset):
         for i in range(size):
             self.data.append(t.FloatTensor(img + np.random.rand(1, 28, 28) * bias * 2 - bias) / 255)
         self.label = [label] * size
+    def __getitem__(self, index):
+        return self.data[index], self.label[index]
+    def __len__(self):
+        return len(self.data)
+
+class pattenKeyposionDataset(t.utils.data.Dataset):
+    def __init__(self, dataList, labelList):
+        self.data = []
+        for i in range(len(dataList)):
+            self.data.append(t.FloatTensor(dataList[i]))
+        self.label = labelList
     def __getitem__(self, index):
         return self.data[index], self.label[index]
     def __len__(self):
@@ -30,10 +41,30 @@ def getStandardData(dataPath):
     return train_data, test_data
 
 def getInstanceKeyPoisonData(imgPath = 'x.jpg',imglabel = 8, trainSize = 5, testSize = 20, trainBias = 5, testBias = 5):
-    trainSet = posionDataset(imgPath = 'x.jpg', \
+    trainSet = InstanceKeyposionDataset(imgPath = 'x.jpg', \
         label = imglabel,size = trainSize, bias = 5)
-    testSet = posionDataset(imgPath = 'x.jpg', \
+    testSet = InstanceKeyposionDataset(imgPath = 'x.jpg', \
         label = imglabel,size = testSize, bias = 20)
+    trainData=DataLoader(trainSet,batch_size=64,shuffle=True)
+    testData=DataLoader(testSet,batch_size=128,shuffle=False)
+    return trainData, testData
+
+def getPatternKeyPosionData(alpha, oriTrainData, oriTestData, imglabel, pattenPath, trainSize = 100):
+    trainDataList = []
+    trainLabelList = []
+    testDataList = []
+    testLabelList = []
+    patten = cv2.resize(cv2.imread(pattenPath, 0), (28, 28)).reshape(1, 28, 28).astype(np.float32) / 255
+    for i in range(trainSize):
+        trainDataList.append(oriTrainData.dataset[i][0] * (1 - alpha) + patten * alpha)
+        trainDataList.append(oriTrainData.dataset[i][0])
+        trainLabelList.append(imglabel)
+        trainLabelList.append(oriTrainData.dataset[i][1])
+    for i in range(len(oriTestData.dataset)):
+        testDataList.append(oriTestData.dataset[i][0] * (1 - alpha) + patten * alpha)
+        testLabelList.append(imglabel)
+    trainSet = pattenKeyposionDataset(dataList = trainDataList, labelList = trainLabelList)
+    testSet = pattenKeyposionDataset(dataList = testDataList, labelList = testLabelList)
     trainData=DataLoader(trainSet,batch_size=64,shuffle=True)
     testData=DataLoader(testSet,batch_size=128,shuffle=False)
     return trainData, testData
